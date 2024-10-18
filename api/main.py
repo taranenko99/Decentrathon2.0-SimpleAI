a41f7325d53941ae12470c2eedf04a7b6e51d084
AnatoliyShevchenko <genitalgrinder90@outlook.com>
2024-10-18 20:20:31 +0500

    make views

--- api/main.py
+++ api/main.py
@@ -3,21 +3,27 @@ import uvicorn
 import asyncio
 
 # Local
-from src.settings.base import logger
+from src.settings.base import logger, app
+from src.v1.views import Registration, ForDoctors, ViewUsers
 
 
 async def main():
+    app.include_router(Registration().router)
+    app.include_router(ForDoctors().router)
+    app.include_router(ViewUsers().router)
     config = uvicorn.Config(
-        app="main:app", host="0.0.0.0", 
+        app=app, host="0.0.0.0", 
         port=8000
     )
     server = uvicorn.Server(config=config)
-    logger.info(msg="SERVER STARTED")
+    logger.info("SERVER STARTED")
     await server.serve()
 
 
 if __name__ == "__main__":
     try:
         asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info("Shutdown server")
     except Exception as e:
         logger.error(f"Shutdown server: {e.__cause__}")
