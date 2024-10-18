6871e922e4f1ff202fbce784f1a94ada6b2ce773
dantepalacio <5562707@mail.ru>
2024-10-18 19:43:03 +0500

    testing

--- llm/test.py
+++ llm/test.py
@@ -12,15 +12,17 @@ chat_history_redis = RedisChatMessageHistory(session_id=chat_history_key,
 chat_chain = get_chain()
 
 a = 0
+
+embeddings = OpenAIEmbeddings()
+
 query = input('Как ваше самочувствие? ')
-db = create_vector_db(r'vector_db/symptoms.txt')
+db = FAISS.load_local(r"faiss_index", embeddings, allow_dangerous_deserialization=True)
+# create_vector_db(r'vector_db/symptoms.txt')
 retriever = db.as_retriever()
 docs = retriever.invoke(query)
 context = docs[0].page_content
 # print(context)
 
-# first_question = generate_question(context)
-# print(first_question)
 
 while True:
     if a == 0:
@@ -33,5 +35,12 @@ while True:
         query_b = input('your answer: ')
         response = chat_chain.invoke({"input": query_b,'context':context, 'chat_history':chat_history_redis.messages[-5:]})
         answer = response.content
+        
+        status_answer = binary_classify(answer)
+        
+        result = {'text':answer,'status':status_answer}
+        print(result)
+
         add_message_to_redis(ai_answer=answer, user_query=query,telegram_id=chat_history_key)
-        print(response.content)
+        
+        # print(response.content)
