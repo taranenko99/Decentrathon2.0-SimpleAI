599feb18c40bcab2b7945d03290a5a4a1ccc2522
AnatoliyShevchenko <genitalgrinder90@outlook.com>
2024-10-18 17:13:23 +0500

    make models to db

--- api/src/db/models.py
+++ api/src/db/models.py
@@ -1,11 +1,72 @@
-# SQL
-from sqlalchemy import BigInteger, String, ForeignKey
+# SQLAlchemy
+from sqlalchemy import BigInteger, String, ForeignKey, Text, Date
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import (
     DeclarativeBase, Mapped, mapped_column, relationship
 )
 
+# Python
+from datetime import date
+
 
 class Base(AsyncAttrs, DeclarativeBase):
     """Base model as parent for other models."""
     pass
+
+
+class Doctors(Base):
+    __tablename__ = "doctors"
+
+    id: Mapped[int] = mapped_column(
+        BigInteger, primary_key=True, unique=True
+    )
+    telegram_id: Mapped[int] = mapped_column(
+        BigInteger, unique=True, nullable=True
+    )
+    individual_number: Mapped[str] = mapped_column(String(12))
+
+    patients: Mapped[list["Patients"]] = relationship(
+        back_populates="doctor", lazy="selectin", 
+        cascade="all, delete-orphan"
+    )
+
+
+class Patients(Base):
+    __tablename__ = "patients"
+
+    id: Mapped[int] = mapped_column(
+        BigInteger, primary_key=True, unique=True
+    )
+    telegram_id: Mapped[int] = mapped_column(
+        BigInteger, unique=True, nullable=True
+    )
+    individual_number: Mapped[str] = mapped_column(String(12))
+    doctor_id: Mapped[int] = mapped_column(
+        ForeignKey("doctors.id", ondelete="CASCADE", onupdate="CASCADE")
+    )
+    doctor: Mapped[Doctors] = relationship(
+        back_populates="patients", lazy="selectin"
+    )
+
+    tests: Mapped[list["PatientTests"]] = relationship(
+        back_populates="patient", lazy="selectin",
+        cascade="all, delete-orphan"
+    )
+
+
+class PatientTests(Base):
+    __tablename__ = "tests"
+
+    id: Mapped[int] = mapped_column(
+        BigInteger, primary_key=True, unique=True
+    )
+    patient_id: Mapped[int] = mapped_column(
+        ForeignKey("patients.id", ondelete="CASCADE"), nullable=False
+    )
+    title: Mapped[str] = mapped_column(String(255))
+    result: Mapped[str] = mapped_column(Text)
+    date_from: Mapped[date] = mapped_column(Date)
+
+    patient: Mapped[Patients] = relationship(
+        back_populates="tests", lazy="selectin"
+    )
